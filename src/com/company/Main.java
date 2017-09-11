f624a30d0ba42a4b177587e7d382b86df8b9a6e4
Yao Yuan <opal_obsidian@hotmail.com>
2017-09-11 17:46:06 +0100

    finished triangle exercise

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -10,12 +10,34 @@ public class Main {
         for (int i = 0; i < n; i++) {
             System.out.print("*");
         }
+        System.out.println("");
+    }
+
+    public static void drawVerticalLine(int n) {
+        for (int i = 0; i < n; i++) {
+            System.out.println("*");
+        }
+    }
+
+    public static void drawRightTriangle(int n) {
+        for (int i = 1; i <= n; i++) {
+            drawHorizontalLine(i);
+        }
+
     }
 
     public static void main(String[] args) {
+        int n = Integer.parseInt(args[0]);
 
         drawAsterisk();
-        drawHorizontalLine(Integer.parseInt(args[0]));
+        System.out.println("draw horizontal line");
+        drawHorizontalLine(n);
+
+        System.out.println("draw vertical line");
+        drawVerticalLine(n);
+
+        System.out.println("draw right triangle");
+        drawRightTriangle(n);
 
     }
 }
